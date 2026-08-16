cf7da507d74c640d3ecc3a13eada8885756e4206
anvar-muzaffarli <prof.anvar2013@gmail.com>
2026-08-16 13:37:25 +0400

    bu sefer vallahi bitdi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,8 +30,8 @@ const Header: React.FC = () => {
                 ref={mobileMenuDivi}
                 className="transition-all duration-1000 ease-in-out mobile-menu-section bg-[#23201E] opacity-[0.9] h-screen fixed z-999999 w-0  overflow-x-hidden top-0 left-0 lg:hidden">
                 <div className="flex justify-between p-5">
-                    <img src="./public/images/mobile/close-icon.svg" alt="" onClick={navbariBagla} className="cursor-pointer" />
-                    <img src="./public/images/mobile/basket.svg" alt="" />
+                    <img src="/images/mobile/close-icon.svg" alt="" onClick={navbariBagla} className="cursor-pointer" />
+                    <img src="/images/mobile/basket.svg" alt="" />
                 </div>
 
                 <nav className="mobile-links text-white p-5 flex flex-col text-[22px] gap-6 py-25">
@@ -47,11 +47,11 @@ const Header: React.FC = () => {
 
 
             <header className="header px-2.5 md:px-10 py-10 flex justify-between items-center">
-                <Link to="/"><img src="./public/images/header/logo.svg" alt="" className="logo hidden lg:flex" /> </Link>
+                <Link to="/"><img src="/images/header/logo.svg" alt="" className="logo hidden lg:flex" /> </Link>
 
                 <div className="mobile-section flex items-center gap-3.5 md:gap-6 lg:hidden ">
-                    <img src="./public/images/header/bars.svg" alt="" onClick={navbariAc}   className="cursor-pointer" />
-                    <img src="./public/images/header/mobile-logo.svg" alt="" />
+                    <img src="/images/header/bars.svg" alt="" onClick={navbariAc}   className="cursor-pointer" />
+                    <img src="/images/header/mobile-logo.svg" alt="" />
                 </div>
 
                 <nav className="nav-links hidden lg:flex lg:gap-10 lg:items-center">
@@ -61,13 +61,13 @@ const Header: React.FC = () => {
                 </nav>
 
                 <div className="search-bar md:w-[90%] lg:w-[30%] flex justify-center items-center gap-7.5">
-                    <img src="./public/images/header/search.svg" alt="" />
+                    <img src="/images/header/search.svg" alt="" />
                     <input type="text" className="hidden md:flex md:w-[60%] lg:w-full" />
                 </div>
 
                 <div className="header-right-side flex gap-3.5 md:gap-6">
-                    <img src="./public/images/header/user.svg" alt="" />
-                    <img src="./public/images/header/basket.svg" alt="" />
+                    <img src="/images/header/user.svg" alt="" />
+                    <img src="/images/header/basket.svg" alt="" />
                 </div>
 
             </header>
